574ee026f56610010fb9765a1e73720c2b2de34a
Carlos Cáceres Watchman Door <ccaceres@watchman.es>
2019-10-24 14:37:30 +0200

    First commit

---
+++ WDCommon.framework/Headers/WDCommon.h
@@ -0,0 +1,19 @@
+//
+//  WDCommon.h
+//  WDCommon
+//
+//  Created by Carlos Cáceres González on 07/03/2019.
+//  Copyright © 2019 Watchman Door S.L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WDCommon.
+FOUNDATION_EXPORT double WDCommonVersionNumber;
+
+//! Project version string for WDCommon.
+FOUNDATION_EXPORT const unsigned char WDCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WDCommon/PublicHeader.h>
+
+
